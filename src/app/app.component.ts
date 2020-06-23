595f169f26bc5d4476e3af32e0b0aead72b4d639
Chien Nguyen Le Minh <chien.nguyen@SAI-IT.COM>
2020-06-23 12:26:18 +0700

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularBegin2020';
+  title = 'bbbbbbbbbbb AngularBegin2020';
+  title2 = 'AngularBegin2020';
 }
